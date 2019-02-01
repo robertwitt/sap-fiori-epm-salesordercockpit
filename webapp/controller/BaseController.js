98d0978d3d54bdfa71433060864e5c68c06d9307
Robert Witt <robert.witt.rw@gmail.com>
2019-02-01 17:14:34 +0000

    Create constants file and hook it into the BaseController.

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,88 +1,91 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	], function (Controller) {
-		"use strict";
+	"sap/ui/core/mvc/Controller",
+	"de/robertwitt/demo/epm/salesordercockpit/model/constants"
+], function (Controller, constants) {
+	"use strict";
 
-		return Controller.extend("de.robertwitt.demo.epm.salesordercockpit.controller.BaseController", {
-			/**
-			 * Convenience method for accessing the router.
-			 * @public
-			 * @returns {sap.ui.core.routing.Router} the router for this component
-			 */
-			getRouter : function () {
-				return sap.ui.core.UIComponent.getRouterFor(this);
-			},
+	return Controller.extend("de.robertwitt.demo.epm.salesordercockpit.controller.BaseController", {
 
-			/**
-			 * Convenience method for getting the view model by name.
-			 * @public
-			 * @param {string} [sName] the model name
-			 * @returns {sap.ui.model.Model} the model instance
-			 */
-			getModel : function (sName) {
-				return this.getView().getModel(sName);
-			},
+		constants: constants,
 
-			/**
-			 * Convenience method for setting the view model.
-			 * @public
-			 * @param {sap.ui.model.Model} oModel the model instance
-			 * @param {string} sName the model name
-			 * @returns {sap.ui.mvc.View} the view instance
-			 */
-			setModel : function (oModel, sName) {
-				return this.getView().setModel(oModel, sName);
-			},
+		/**
+		 * Convenience method for accessing the router.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter: function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
 
-			/**
-			 * Getter for the resource bundle.
-			 * @public
-			 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
-			 */
-			getResourceBundle : function () {
-				return this.getOwnerComponent().getModel("i18n").getResourceBundle();
-			},
+		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @param {string} [sName] the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel: function (sName) {
+			return this.getView().getModel(sName);
+		},
 
-			/**
-			 * Event handler when the share by E-Mail button has been clicked
-			 * @public
-			 */
-			onShareEmailPress : function () {
-				var oViewModel = (this.getModel("objectView") || this.getModel("worklistView"));
-				sap.m.URLHelper.triggerEmail(
-					null,
-					oViewModel.getProperty("/shareSendEmailSubject"),
-					oViewModel.getProperty("/shareSendEmailMessage")
-				);
-			},
+		/**
+		 * Convenience method for setting the view model.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel: function (oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
 
-			/**
-			* Adds a history entry in the FLP page history
-			* @public
-			* @param {object} oEntry An entry object to add to the hierachy array as expected from the ShellUIService.setHierarchy method
-			* @param {boolean} bReset If true resets the history before the new entry is added
-			*/
-			addHistoryEntry: (function() {
-				var aHistoryEntries = [];
+		/**
+		 * Getter for the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
 
-				return function(oEntry, bReset) {
-					if (bReset) {
-						aHistoryEntries = [];
-					}
+		/**
+		 * Event handler when the share by E-Mail button has been clicked
+		 * @public
+		 */
+		onShareEmailPress: function () {
+			var oViewModel = (this.getModel("objectView") || this.getModel("worklistView"));
+			sap.m.URLHelper.triggerEmail(
+				null,
+				oViewModel.getProperty("/shareSendEmailSubject"),
+				oViewModel.getProperty("/shareSendEmailMessage")
+			);
+		},
 
-					var bInHistory = aHistoryEntries.some(function(entry) {
-						return entry.intent === oEntry.intent;
+		/**
+		 * Adds a history entry in the FLP page history
+		 * @public
+		 * @param {object} oEntry An entry object to add to the hierachy array as expected from the ShellUIService.setHierarchy method
+		 * @param {boolean} bReset If true resets the history before the new entry is added
+		 */
+		addHistoryEntry: (function () {
+			var aHistoryEntries = [];
+
+			return function (oEntry, bReset) {
+				if (bReset) {
+					aHistoryEntries = [];
+				}
+
+				var bInHistory = aHistoryEntries.some(function (entry) {
+					return entry.intent === oEntry.intent;
+				});
+
+				if (!bInHistory) {
+					aHistoryEntries.push(oEntry);
+					this.getOwnerComponent().getService("ShellUIService").then(function (oService) {
+						oService.setHierarchy(aHistoryEntries);
 					});
+				}
+			};
+		})()
+	});
 
-					if (!bInHistory) {
-						aHistoryEntries.push(oEntry);
-						this.getOwnerComponent().getService("ShellUIService").then(function(oService) {
-							oService.setHierarchy(aHistoryEntries);
-						});
-					}
-				};
-			})()
-		});
-
-	}
-);
+});
---
+++ webapp/model/constants.js
@@ -0,0 +1,18 @@
+sap.ui.define([], function () {
+	"use strict";
+
+	return {
+
+		salesOrder: {
+
+			lifecycleStatus: {
+				new: "N",
+				inProgress: "P",
+				closed: "C",
+				canceled: "X"
+			}
+
+		}
+
+	};
+});
